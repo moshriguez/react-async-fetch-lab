56f620782ef08419db421f4228d9fff04fc8d2f3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-03 10:13:27 -0700

    commit tree 7f161e0c07920c0d33e886a14a246d7065456036

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React from 'react'
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            astros: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(json => {
+            this.setState({
+                astros: json
+            })
+        })
+    }
+
+    render() {
+        return <div></div>
+    }
+}
+
+export default App
